f4a0e0e4e8572183accc65ab778446caf27f32a4
Chiesa14 <remychiesa14@gmail.com>
2023-10-10 22:00:50 +0200

    usermodel

---
+++ backend/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    location: { type: String, required: true },
+    password: { type: String, required: true },
+    Image: {
+      type: String,
+      required: true,
+      default: "./uploads/userDefault.pnng",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("users", UserSchema);
